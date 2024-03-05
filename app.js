b3cf6a5402f0366198343605822ac08c5be6b6b6
Waqas Ahmad <137182426+Waqasahmed15@users.noreply.github.com>
2024-03-05 20:25:14 +0500

    prompt

---
+++ app.js
@@ -0,0 +1,13 @@
+var userName = prompt()
+// alert(userName);
+if (userName == "munir"){
+    alert("mil gya")
+}else if(userName == "kamran"){
+    alert("mil gya")
+}else if(userName == "sajjad"){
+    alert("mil gya")
+}else if(userName == "raza"){
+    alert("mil gya")
+}else{
+    alert("nahi mila")
+}
